4e7ae227b9fb0861731ac36c23108769a611c1f1
DabbacheDjawad <djawadmne@gmail.com>
2024-11-09 19:57:36 +0100

    some react basic mini projects

---
+++ Food Recipe App/src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Details from "./pages/Details";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
